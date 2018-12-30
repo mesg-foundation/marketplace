fe7164efecbd77aacedbbff7ae89450deb0fcb42
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-12-30 18:14:11 +0700

    update create service task

--- service/tasks/createService.js
+++ service/tasks/createService.js
@@ -1,45 +1,45 @@
-module.exports = ({ MESG, contract, account, web3 }) => async ({ sid, hash, location, price }, { success, error }) => {
-  try {
-    console.log(account, account.address)
-    let serviceAddr = await contract.methods.services(web3.utils.fromAscii(sid)).call({ from: account.address })
-    console.log(1)
-    if (!serviceAddr) {
-      console.log(2)
-      const signedTransaction = await account.signTransaction({
-        to: contract.options.address,
-        data: contract.methods.createService(web3.utils.fromAscii(sid)).encodeABI(),
-        gas: 1200000,
-        value: 0,
-        gasPrice: await web3.eth.getGasPrice()
-      })
-      console.log(3, signTransaction)
-      const receipt = await new Promise((resolve, reject) => web3.eth.sendSignedTransaction(signedTransaction.rawTransaction)
-        .on('receipt', resolve)
-        .on('error', reject)
-      )
-      console.log(4, receipt)
-      serviceAddr = receipt
-    }
+const { abi } = require("../Service.json")
 
-    console.log(5)
+const getOrCreateService = async (web3, contract, account, sid) => {
+  const serviceAddr = await contract.methods.serviceContracts(web3.utils.fromAscii(sid)).call()
+  if (serviceAddr) {
+    return serviceAddr
+  }
+  const signedTransaction = await account.signTransaction({
+    to: contract.options.address,
+    data: contract.methods.createService(web3.utils.fromAscii(sid)).encodeABI(),
+    gas: 1200000,
+    value: 0,
+    gasPrice: await web3.eth.getGasPrice()
+  })
+  const receipt = await new Promise((resolve, reject) => web3.eth.sendSignedTransaction(signedTransaction.rawTransaction)
+    .on('receipt', resolve)
+    .on('error', reject)
+  )
+  return receipt
+}
+
+module.exports = ({ contract, account, web3 }) => async ({ sid, hash, location, price }, { success, error }) => {
+  try {
+    const serviceAddr = await getOrCreateService(web3, contract, account, sid)
+    const serviceContract = new web3.eth.Contract(abi, serviceAddr)
+    const parameters = [
+      web3.utils.fromAscii(hash),
+      web3.utils.fromAscii(location),
+      price
+    ]
     const signedTransaction = await account.signTransaction({
-      to: service.options.address,
-      data: contract.methods.createService(web3.utils.fromAscii(sid)).encodeABI(),
+      to: serviceAddr,
+      data: serviceContract.methods.createVersion(...parameters).encodeABI(),
       gas: 1200000,
       value: 0,
       gasPrice: await web3.eth.getGasPrice()
     })
-    console.log(6, signTransaction)
-    const receipt = await new Promise((resolve, reject) => web3.eth.sendSignedTransaction(signedTransaction.rawTransaction)
+    await new Promise((resolve, reject) => web3.eth.sendSignedTransaction(signedTransaction.rawTransaction)
       .on('receipt', resolve)
       .on('error', reject)
     )
-    console.log(7, receipt)
-    return success({
-      sid: receipt.sid,
-      hash: receipt.hash,
-      location: receipt.location
-    })
+    return success({ sid, hash, location })
   } catch (e) {
     return error({ error: e.toString() })
   }
