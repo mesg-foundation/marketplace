686a2a188666f85774c68c1aa0478f0e501ba626
krhubert <krhubert@gmail.com>
2019-01-08 23:39:09 +0100

    Next

---
+++ contracts/DBMesgService.sol
@@ -0,0 +1,58 @@
+pragma solidity >=0.5.2 <0.6.0;
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract DBMesgService is Ownable {
+  constructor() public {}
+
+  struct Version {
+    bytes16 hash;
+    bytes url;
+  }
+
+  struct Service {
+    bytes32 sid;
+    Version[] versions;
+  }
+
+  Service[] public services;
+
+  function addService (bytes32 sid) public onlyOwner {
+    services.length++;
+    Service storage s = services[services.length - 1];
+    s.sid = sid;
+  }
+
+  function addServiceVersion (bytes32 sid, bytes16 hash, bytes memory url) public onlyOwner {
+    for (uint i = 0; i < services.length; i++) {
+      if (services[i].sid == sid) {
+        services[i].versions.push(Version({
+          hash: hash, 
+          url: url
+        }));
+        break;
+      }
+    }
+  }
+
+  function getServicesCount() public view returns(uint servicesCount) {
+    return services.length;
+  }
+
+  function getServiceVersion(bytes32 sid, uint index) public view returns(bytes16 hash, bytes memory url) {
+    for (uint i = 0; i < services.length; i++) {
+      if (services[i].sid == sid) {
+        return (services[i].versions[index].hash, services[i].versions[index].url);
+      }
+    }
+  }
+
+  function getServiceVersionsCount(bytes32 sid) public view returns(uint serviceVersionsCount) {
+    for (uint i = 0; i < services.length; i++) {
+      if (services[i].sid == sid) {
+        return services[i].versions.length;
+      }
+    }
+    return 0;
+  }
+}
--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,82 +1,90 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-import "openzeppelin-solidity/contracts/utils/Address.sol";
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-import "openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
+// import "openzeppelin-solidity/contracts/utils/Address.sol";
+// import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+// import "openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
 
-contract Service is Ownable, Pausable {
-    bytes32 public sid;
-    bytes32 public latest;
-    mapping (bytes32 => Version) public versions;
-    mapping (bytes32 => mapping(address => bool)) private _versionAccess;
+// contract Service is Ownable, Pausable {
+//     bytes32 public sid;
+//     bytes32 public latest;
+//     mapping (bytes32 => Version) public versions;
+//     mapping (bytes32 => mapping(address => bool)) private _versionAccess;
 
-    struct Version {
-        bytes32 id;
-        bytes32 location;
-        uint256 price;
-        bool _exists;
-    }
+//     struct Version {
+//         bytes32 id;
+//         bytes32 location;
+//         uint256 price;
+//         bool _exists;
+//     }
 
-    event VersionCreated(bytes32 sid, bytes32 id, bytes32 location, uint256 price);
+//     event VersionCreated(bytes32 sid, bytes32 id, bytes32 location, uint256 price);
 
-    constructor(address creator, bytes32 _sid) Ownable() Pausable() public {
-        sid = _sid;
-        transferOwnership(creator);
-        addPauser(creator);
-        renouncePauser();
-    }
+//     constructor(address creator, bytes32 _sid) Ownable() Pausable() public {
+//         sid = _sid;
+//         transferOwnership(creator);
+//         addPauser(creator);
+//         renouncePauser();
+//     }
 
-    function createVersion(bytes32 versionHash, bytes32 location, uint256 price) public onlyOwner whenNotPaused {
-        require(!versions[versionHash]._exists, "This version already exists");
-        versions[versionHash] = Version({
-            id: versionHash,
-            location: location,
-            price: price,
-            _exists: true
-        });
-        latest = versionHash;
-        emit VersionCreated(sid, versionHash, location, price);
-        return;
-    }
+//     function createVersion(bytes32 versionHash, bytes32 location, uint256 price) public onlyOwner whenNotPaused {
+//         require(!versions[versionHash]._exists, "This version already exists");
+//         versions[versionHash] = Version({
+//             id: versionHash,
+//             location: location,
+//             price: price,
+//             _exists: true
+//         });
+//         latest = versionHash;
+//         emit VersionCreated(sid, versionHash, location, price);
+//         return;
+//     }
 
-    function requestAccess(bytes32 version) public whenNotPaused {
-        require(!_versionAccess[version][msg.sender], "You already have access to this version of the service");
-        _versionAccess[version][msg.sender] = true;
-        return;
-    }
+//     function requestAccess(bytes32 version) public whenNotPaused {
+//         require(!_versionAccess[version][msg.sender], "You already have access to this version of the service");
+//         _versionAccess[version][msg.sender] = true;
+//         return;
+//     }
 
-    function hasAccessToVersion(bytes32 version, address user) public view returns (bool) {
-        return _versionAccess[version][user];
-    }
+//     function hasAccessToVersion(bytes32 version, address user) public view returns (bool) {
+//         return _versionAccess[version][user];
+//     }
 
-    function isContract() public view returns (bool) {
-  		uint32 size;
-      address a = msg.sender;
-  		assembly {
-    		size := extcodesize(a)
-		  }
-  		return (size > 0);
-		}
-}
+//     function isContract() public view returns (bool) {
+//   		uint32 size;
+//       address a = msg.sender;
+//   		assembly {
+//     		size := extcodesize(a)
+// 		  }
+//   		return (size > 0);
+// 		}
+// }
 
-contract Marketplace is Ownable, Pausable {
-    using Address for address;
+// contract Marketplace is Ownable, Pausable {
+//   struct Version {
+//     bytes32 location;
+//     bytes32 version;
+//   }
 
-    mapping (bytes32 => Service) public serviceContracts;
-    bytes32[] public services;
-    uint256 public totalServices;
+//   struct Service {
+//     address owner;
+//     bytes32 public sid;
+//     Version[] public versions;
+//   }
+
+//   mapping (bytes32 => Service) public services;
+//   bytes32[] public servicessid;
     
-    event ServiceCreated(bytes32 sid, address serviceAddress);
+//   event ServiceCreated(bytes32 sid, address serviceAddress);
 
-    constructor() Ownable() Pausable() public {}
+//   constructor() Ownable() Pausable() public {}
 
-    function createService(bytes32 sid) public whenNotPaused returns (address) {
-        if (!serviceContracts[sid].isContract()) {
-        	serviceContracts[sid] = new Service(msg.sender, sid);
-        	services.push(sid);
-        	totalServices = totalServices + 1;
-        	emit ServiceCreated(sid, address(serviceContracts[sid]));
-        }
-        return address(serviceContracts[sid]);
-    }
-}
+//   function createService(bytes32 sid) public whenNotPaused returns (address) {
+//     if (!serviceContracts[sid].isContract()) {
+//     	serviceContracts[sid] = new Service(msg.sender, sid);
+//     	services.push(sid);
+//     	totalServices = totalServices + 1;
+//     	emit ServiceCreated(sid, address(serviceContracts[sid]));
+//     }
+//     return address(serviceContracts[sid]);
+//   }
+// }
