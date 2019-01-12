2d3b44bf6abd8ee5341a94e90449b6e94a432406
Nicolas Mahe <nicolas@mahe.me>
2019-01-12 19:24:39 +0700

    improve tests

--- test/marketplace.js
+++ test/marketplace.js
@@ -13,6 +13,72 @@ const hexToNumber = x => web3.utils.hexToNumber(x)
 const BN = x => new web3.utils.BN(x)
 const isBN = x => web3.utils.isBN(x)
 
+// Errors from contract
+const errorServiceOwner = 'Service owner is not the same as the sender'
+const errorServiceSidAlreadyUsed = 'Sid is already used'
+const errorServiceNotFound = 'Service not found'
+const errorServiceVersionNotFound = 'Version not found'
+const errorServicePaymentNotFound = 'Payment not found'
+
+// Assert functions
+
+// Service
+const assertEventServiceCreated = (tx, serviceIndex, sid, price, owner) => {
+  truffleAssert.eventEmitted(tx, 'ServiceCreated')
+  const event = tx.logs[0].args
+  assert.isTrue(event.serviceIndex.eq(BN(serviceIndex)))
+  assert.equal(hexToAscii(event.sid), sid)
+  assert.equal(event.owner, owner)
+  assert.equal(event.price, price)
+}
+const assertService = (service, sid, price, owner) => {
+  assert.equal(service.owner, owner)
+  assert.equal(hexToAscii(service.sid), sid)
+  assert.equal(service.price, price)
+}
+const assertEventServicePriceChanged = (tx, serviceIndex, sid, previousPrice, newPrice) => {
+  truffleAssert.eventEmitted(tx, 'ServicePriceChanged')
+  const event = tx.logs[0].args
+  assert.isTrue(event.serviceIndex.eq(BN(serviceIndex)))
+  assert.equal(hexToAscii(event.sid), sid)
+  assert.equal(event.previousPrice, previousPrice)
+  assert.equal(event.newPrice, newPrice)
+}
+const assertEventServiceOwnershipTransferred = (tx, serviceIndex, sid, previousOwner, newOwner) => {
+  truffleAssert.eventEmitted(tx, 'ServiceOwnershipTransferred')
+  const event = tx.logs[0].args
+  assert.isTrue(event.serviceIndex.eq(BN(serviceIndex)))
+  assert.equal(hexToAscii(event.sid), sid)
+  assert.equal(event.previousOwner, previousOwner)
+  assert.equal(event.newOwner, newOwner)
+}
+
+// Service version
+const assertEventServiceVersionCreated = (tx, serviceIndex, versionHash, versionUrl) => {
+  truffleAssert.eventEmitted(tx, 'ServiceVersionCreated')
+  const event = tx.logs[0].args
+  assert.isTrue(event.serviceIndex.eq(BN(serviceIndex)))
+  assert.equal(event.hash, versionHash)
+  assert.equal(hexToAscii(event.url), versionUrl)
+}
+const assertServiceVersion = (version, versionHash, versionUrl) => {
+  assert.equal(version.hash, versionHash)
+  assert.equal(hexToAscii(version.url), versionUrl)
+}
+
+// Service payment
+const assertEventServicePaid = (tx, serviceIndex, sid, price, purchaser) => {
+  truffleAssert.eventEmitted(tx, 'ServicePaid')
+  const event = tx.logs[0].args
+  assert.isTrue(event.serviceIndex.eq(BN(serviceIndex)))
+  assert.equal(hexToAscii(event.sid), sid)
+  assert.equal(event.purchaser, purchaser)
+  assert.equal(event.price, price)
+}
+const assertServicePayment = (payment, purchaser) => {
+  assert.equal(payment, purchaser)
+}
+
 contract('Marketplace', async accounts => {
   const [
     contractOwner,
@@ -20,7 +86,8 @@ contract('Marketplace', async accounts => {
     developer,
     developer2,
     purchaser,
-    purchaser2
+    purchaser2,
+    other
   ] = accounts
   let marketplace = null
 
@@ -30,8 +97,38 @@ contract('Marketplace', async accounts => {
     })
 
     it('original owner should have the ownership', async () => {
-      assert.equal(await marketplace.owner.call(), contractOwner)
-      assert.equal(await marketplace.isPauser.call(contractOwner), true)
+      assert.isTrue(await marketplace.isOwner({ from: contractOwner }))
+    })
+
+    it('other should not have the ownership', async () => {
+      assert.isFalse(await marketplace.isOwner({ from: other }))
+    })
+
+    it('should transfer ownership', async () => {
+      const tx = await marketplace.transferOwnership(contractOwner2, { from: contractOwner })
+      truffleAssert.eventEmitted(tx, 'OwnershipTransferred')
+    })
+
+    it('original owner should not have the ownership', async () => {
+      assert.isFalse(await marketplace.isOwner({ from: contractOwner }))
+    })
+
+    it('new owner should have the ownership', async () => {
+      assert.isTrue(await marketplace.isOwner({ from: contractOwner2 }))
+    })
+  })
+
+  describe('contract pauser', async () => {
+    before(async () => {
+      marketplace = await Marketplace.new({ from: contractOwner })
+    })
+
+    it('original owner should be pauser', async () => {
+      assert.isTrue(await marketplace.isPauser(contractOwner))
+    })
+
+    it('other should not be pauser', async () => {
+      assert.isFalse(await marketplace.isPauser(other))
     })
 
     it('should add pauser role', async () => {
@@ -44,14 +141,12 @@ contract('Marketplace', async accounts => {
       truffleAssert.eventEmitted(tx, 'PauserRemoved')
     })
 
-    it('should transfer ownership', async () => {
-      const tx = await marketplace.transferOwnership(contractOwner2, { from: contractOwner })
-      truffleAssert.eventEmitted(tx, 'OwnershipTransferred')
+    it('previous owner should not be pauser', async () => {
+      assert.isFalse(await marketplace.isPauser(contractOwner))
     })
 
-    it('new owner should have the ownership', async () => {
-      assert.equal(await marketplace.owner.call(), contractOwner2)
-      assert.equal(await marketplace.isPauser.call(contractOwner2), true)
+    it('new owner should be pauser', async () => {
+      assert.isTrue(await marketplace.isPauser(contractOwner2))
     })
   })
 
@@ -73,6 +168,8 @@ contract('Marketplace', async accounts => {
       truffleAssert.eventEmitted(tx, 'Unpaused')
       assert.equal(await marketplace.paused(), false)
     })
+
+    // TODO: add ALL function that use whenNotPaused modifier
   })
 
   describe('create service', async () => {
@@ -83,103 +180,120 @@ contract('Marketplace', async accounts => {
       hash: '0xa666c79d6eccdcdd670d25997b5ec7d3f7f8fc94',
       url: 'https://download.com/core.tar'
     }
+    const version2 = {
+      hash: '0xb444c79d6eccdcdd670d25997b5ec7d3f7f8fc94',
+      url: 'https://get.com/core.tar'
+    }
 
     before(async () => {
       marketplace = await Marketplace.new()
     })
 
-    it('should create a service', async () => {
-      const tx = await marketplace.createService(asciiToHex(sid), price,
-        { from: developer }
-      )
-      truffleAssert.eventEmitted(tx, 'ServiceCreated')
-      const event = tx.logs[0].args
-      assert.isTrue(isBN(event.serviceIndex))
-      assert.isTrue(event.serviceIndex.eq(BN(0)))
-      assert.equal(hexToAscii(event.sid), sid)
-      assert.equal(event.owner, developer)
+    describe('service creation', async () => {
+      it('should create a service', async () => {
+        const tx = await marketplace.createService(asciiToHex(sid), price, { from: developer })
+        assertEventServiceCreated(tx, 0, sid, price, developer)
+      })
+
+      it('should have one service', async () => {
+        const service = await marketplace.services(0)
+        assertService(service, sid, price, developer)
+      })
     })
 
-    it('should have one service', async () => {
-      assert.equal(await marketplace.getServicesCount(), 1)
-      const service = await marketplace.services.call(0)
-      assert.equal(service.owner, developer)
-      assert.equal(hexToAscii(service.sid), sid)
-      assert.equal(service.price, price)
+    describe('service price', async () => {
+      it('should change service price', async () => {
+        const tx = await marketplace.changeServicePrice(0, price2, { from: developer })
+        assertEventServicePriceChanged(tx, 0, sid, price, price2)
+      })
+
+      it('should have changed service price', async () => {
+        assert.equal(await marketplace.getServicesCount(), 1)
+        const service = await marketplace.services(0)
+        assertService(service, sid, price2, developer)
+      })
     })
 
-    it('should change service price', async () => {
-      const tx = await marketplace.changeServicePrice(0, price2,
-        { from: developer }
-      )
-      truffleAssert.eventEmitted(tx, 'ServicePriceChanged')
-      const event = tx.logs[0].args
-      assert.isTrue(isBN(event.serviceIndex))
-      assert.isTrue(event.serviceIndex.eq(BN(0)))
-      assert.equal(hexToAscii(event.sid), sid)
-      assert.equal(event.previousPrice, price)
-      assert.equal(event.newPrice, price2)
+    describe('service version', async () => {
+      it('should create a version', async () => {
+        const tx = await marketplace.createServiceVersion(0, version.hash, asciiToHex(version.url), { from: developer })
+        assertEventServiceVersionCreated(tx, 0, version.hash, version.url)
+      })
+
+      it('should have one version', async () => {
+        assert.equal(await marketplace.getServiceVersionsCount(0), 1)
+        const _version = await marketplace.getServiceVersion(0, 0)
+        assertServiceVersion(_version, version.hash, version.url)
+      })
+
+      it('should create an other version', async () => {
+        const tx = await marketplace.createServiceVersion(0, version2.hash, asciiToHex(version2.url), { from: developer })
+        assertEventServiceVersionCreated(tx, 0, version2.hash, version2.url)
+      })
+
+      it('should have two version', async () => {
+        assert.equal(await marketplace.getServiceVersionsCount(0), 2)
+        // check version
+        const _version = await marketplace.getServiceVersion(0, 0)
+        assertServiceVersion(_version, version.hash, version.url)
+        // check version2
+        const _version2 = await marketplace.getServiceVersion(0, 1)
+        assertServiceVersion(_version2, version2.hash, version2.url)
+      })
     })
 
-    it('should have changed service price', async () => {
-      assert.equal(await marketplace.getServicesCount(), 1)
-      const service = await marketplace.services.call(0)
-      assert.equal(service.owner, developer)
-      assert.equal(hexToAscii(service.sid), sid)
-      assert.equal(service.price, price2)
+    describe('service payment', async () => {
+      it('should pay a service', async () => {
+        const tx = await marketplace.pay(0, { from: purchaser, value: price2 })
+        assertEventServicePaid(tx, 0, sid, price2, purchaser)
+      })
+
+      it('should have paid service', async () => {
+        assert.equal(await marketplace.hasPaid(0, { from: purchaser }), true)
+        const _payment = await marketplace.getServicePayment(0, 0)
+        assertServicePayment(_payment, purchaser)
+      })
     })
 
-    it('should create a version', async () => {
-      const tx = await marketplace.createServiceVersion(0, version.hash, asciiToHex(version.url),
-        { from: developer }
-      )
-      truffleAssert.eventEmitted(tx, 'ServiceVersionCreated')
-      const event = tx.logs[0].args
-      assert.isTrue(web3.utils.isBN(event.serviceIndex))
-      assert.isTrue(event.serviceIndex.eq(BN(0)))
-      assert.equal(event.hash, version.hash)
-      assert.equal(hexToAscii(event.url), version.url)
-    })
+    describe('service ownership', async () => {
+      it('original owner should have the service ownership', async () => {
+        assert.equal(await marketplace.getServicesCount(), 1)
+        assert.isTrue(await marketplace.isServiceOwner(0, { from: developer }))
+      })
 
-    it('should have one version', async () => {
-      assert.equal(await marketplace.getServiceVersionsCount(0), 1)
-      const _version = await marketplace.getServiceVersion(0, 0)
-      assert.equal(_version.hash, version.hash)
-      assert.equal(hexToAscii(_version.url), version.url)
-    })
+      it('other should not have the service ownership', async () => {
+        assert.equal(await marketplace.getServicesCount(), 1)
+        assert.isFalse(await marketplace.isServiceOwner(0, { from: other }))
+      })
 
-    it('should pay a service', async () => {
-      const tx = await marketplace.pay(0,
-        { from: purchaser, value: price2 }
-      )
-      truffleAssert.eventEmitted(tx, 'ServicePaid')
-      const event = tx.logs[0].args
-      assert.isTrue(web3.utils.isBN(event.serviceIndex))
-      assert.isTrue(event.serviceIndex.eq(BN(0)))
-      assert.equal(hexToAscii(event.sid), sid)
-      assert.equal(event.purchaser, purchaser)
-      assert.equal(event.price, price2)
-    })
+      it('should transfer service ownership', async () => {
+        const tx = await marketplace.transferServiceOwnership(0, developer2,
+          { from: developer }
+        )
+        assertEventServiceOwnershipTransferred(tx, 0, sid, developer, developer2)
+      })
 
-    it('should have paid service', async () => {
-      assert.equal(await marketplace.hasPaid(0, {
-        from: purchaser
-      }), true)
-      const _payment = await marketplace.getServicePayment(0, 0)
-      assert.equal(_payment, purchaser)
-    })
+      it('new service owner should have the service ownership', async () => {
+        assert.equal(await marketplace.getServicesCount(), 1)
+        assert.isTrue(await marketplace.isServiceOwner(0, { from: developer2 }))
+      })
 
-    it('should transfer service ownership', async () => {
-      const tx = await marketplace.transferServiceOwnership(0, developer2,
-        { from: developer }
-      )
-      truffleAssert.eventEmitted(tx, 'ServiceOwnershipTransferred')
-      const event = tx.logs[0].args
-      assert.isTrue(web3.utils.isBN(event.serviceIndex))
-      assert.isTrue(event.serviceIndex.eq(BN(0)))
-      assert.equal(hexToAscii(event.sid), sid)
-      assert.equal(event.previousOwner, developer)
-      assert.equal(event.newOwner, developer2)
+      it('original owner should not have the service ownership', async () => {
+        assert.equal(await marketplace.getServicesCount(), 1)
+        assert.isFalse(await marketplace.isServiceOwner(0, { from: developer }))
+      })
+
+      it('original owner should not be able to transfer service ownership', async () => {
+        await truffleAssert.reverts(marketplace.transferServiceOwnership(0, other, { from: developer }), errorServiceOwner)
+      })
+      it('original owner should not be able to change service price', async () => {
+        await truffleAssert.reverts(marketplace.changeServicePrice(0, price, { from: developer }), errorServiceOwner)
+      })
+      it('original owner should not be able to create a service version', async () => {
+        await truffleAssert.reverts(marketplace.createServiceVersion(0, version.hash, asciiToHex(version.url), { from: developer }), errorServiceOwner)
+      })
+
+      // TODO: add ALL function that use onlyServiceOwner modifier
     })
   })
 })
