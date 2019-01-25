c8f9e9b9e72429d5cc8c919602d3e6831a6f245d
Nicolas Mahe <nicolas@mahe.me>
2019-01-25 17:25:55 +0700

    Set a real metadata link

--- migrations/2_marketplace.js
+++ migrations/2_marketplace.js
@@ -13,14 +13,14 @@ module.exports = async (deployer, network) => {
 
     await marketplace.createService(asciiToHex('test-service-0'), 1000)
     console.log('service created')
-    await marketplace.createServiceVersion(0, '0xa666c79d6eccdcdd670d25997b5ec7d3f7f8fc94', asciiToHex('https://download.com/core.tar'))
+    await marketplace.createServiceVersion(0, '0xa666c79d6eccdcdd670d25997b5ec7d3f7f8fc94', asciiToHex('https://raw.githubusercontent.com/mesg-foundation/marketplace/dev/metadata.json'))
     console.log('version created')
-    await marketplace.createServiceVersion(0, '0xb444c79d6eccdcdd670d25997b5ec7d3f7f8fc94', asciiToHex('https://download.com/core.tar'))
+    await marketplace.createServiceVersion(0, '0xb444c79d6eccdcdd670d25997b5ec7d3f7f8fc94', asciiToHex('https://raw.githubusercontent.com/mesg-foundation/marketplace/dev/metadata.json'))
     console.log('version created')
 
     await marketplace.createService(asciiToHex('test-service-1'), 2000)
     console.log('service created')
-    await marketplace.createServiceVersion(1, '0xc5555c79d6eccdcdd670d25997b5ec7d3f7f8fc94', asciiToHex('https://download.com/core.tar'))
+    await marketplace.createServiceVersion(1, '0xc5555c79d6eccdcdd670d25997b5ec7d3f7f8fc94', asciiToHex('https://raw.githubusercontent.com/mesg-foundation/marketplace/dev/metadata.json'))
     console.log('version created')
   }
 }
