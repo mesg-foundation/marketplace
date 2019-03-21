67957121226ca3ac87b1de2d7b3302d996a7c7ba
Nicolas Mahe <nicolas@mahe.me>
2019-03-21 13:48:46 +0700

    Fix migration for staging

--- migrations/2_marketplace.js
+++ migrations/2_marketplace.js
@@ -22,26 +22,12 @@ module.exports = async (deployer, network) => {
     const marketplace = await Marketplace.deployed()
 
     const sid = asciiToHex('test-service-0')
-    const sid2 = asciiToHex('test-service-1')
-    const manifest = asciiToHex('https://raw.githubusercontent.com/mesg-foundation/marketplace/dev/manifest.json')
-    const manifestType = asciiToHex('http')
+    const manifest = asciiToHex('QmfB1GtfjRQGFYAw3r9TKUjUyM3bY4cts9QXd16F8zDFDL')
+    const manifestType = asciiToHex('ipfs')
 
-    await marketplace.createService(sid)
-    console.log('service created')
-    await marketplace.createServiceVersion(sid, '0xa666c79d6eccdcdd670d25997b5ec7d3f7f8fc94', manifest, manifestType)
-    console.log('version created')
-    await marketplace.createServiceVersion(sid, '0xb444c79d6eccdcdd670d25997b5ec7d3f7f8fc94', manifest, manifestType)
-    console.log('version created')
-    await marketplace.createServiceOffer(sid, 100, 60)
-    console.log('offer created')
-    await marketplace.createServiceOffer(sid, 1000, 3600 * 24)
-    console.log('offer created')
-
-    await marketplace.createService(sid2)
-    console.log('service created')
-    await marketplace.createServiceVersion(sid2, '0xc5555c79d6eccdcdd670d25997b5ec7d3f7f8fc94', manifest, manifestType)
-    console.log('version created')
-    await marketplace.createServiceOffer(sid2, 2000, 3600 * 24)
+    await marketplace.publishServiceVersion(sid, manifest, manifestType)
+    console.log('service and version created')
+    await marketplace.createServiceOffer(sid, 2 * 10e18, 60)
     console.log('offer created')
   }
 }
