e290aaa6222e4343d2923764aa2bf49b63c53834
Nicolas Mahe <nicolas@mahe.me>
2019-04-16 17:23:29 +0700

    fix lint

--- contracts/DnsUtils.sol
+++ contracts/DnsUtils.sol
@@ -2,26 +2,26 @@ pragma solidity >=0.5.0 <0.6.0;
 
 library DnsUtils {
   function isDomainName(bytes memory s) internal pure returns (bool) {
-    byte last = '.';
+    byte last = ".";
     bool ok = false;
     uint partlen = 0;
 
     for (uint i = 0; i < s.length; i++) {
       byte c = s[i];
-      if (c >= 'a' && c <= 'z' || c == '_') {
+      if (c >= "a" && c <= "z" || c == "_") {
         ok = true;
         partlen++;
-      } else if (c >= '0' && c <= '9') {
+      } else if (c >= "0" && c <= "9") {
         partlen++;
-      } else if (c == '-') {
+      } else if (c == "-") {
         // byte before dash cannot be dot.
-        if (last == '.') {
+        if (last == ".") {
           return false;
         }
         partlen++;
-      } else if (c == '.') {
+      } else if (c == ".") {
         // byte before dot cannot be dot, dash.
-        if (last == '.' || last == '-') {
+        if (last == "." || last == "-") {
           return false;
         }
         if (partlen > 63 || partlen == 0) {
@@ -33,7 +33,7 @@ library DnsUtils {
       }
       last = c;
     }
-    if (last == '-' || partlen > 63) {
+    if (last == "-" || partlen > 63) {
       return false;
     }
     return ok;
